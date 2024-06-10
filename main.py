2be376d9cf4658e3d1f7c9abc7cefb34379ffe21
Dilexi <dilexilex97@gmail.com>
2024-06-10 20:02:22 +0300

    create function check_for_redirect

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import requests
 
+
+def check_for_redirect(response):
+   if response.history:
+      raise requests.exception.HTTPError
+   
+
+
 for id in range(1,10):
-  url = f"https://tululu.org/txt.php?id={id}"
-
-  response = requests.get(url)
-  response.raise_for_status() 
-
-  filename = f'book{id}.txt'
-  with open(filename, 'wb') as file:
-      file.write(response.content)
+    try:
+        url = f"https://tululu.org/txt.php?id={id}"
+        response = requests.get(url)
+        response.raise_for_status() 
+        check_for_redirect(response)
+        filename = f'book{id}.txt'
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+    except:
+        print('книга не найдена')
