0da9a3b05a2048ffbcaa9559f34538779d951406
Dilexi <dilexilex97@gmail.com>
2024-06-11 12:23:30 +0300

    Подготовил проект к публикации

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import os
 import requests
+import argparse
 from bs4 import BeautifulSoup
 from pathvalidate import sanitize_filename
-import os
 from urllib.parse import urljoin, unquote, urlsplit
 
 
+
 def check_for_redirect(response):
-   if response.history:
-      raise requests.exception.HTTPError
+    if response.history:
+        raise requests.exception.HTTPError
    
+
 def download_txt(url, filename, folder='books/'):
     os.makedirs(folder, exist_ok=True)
     response = requests.get(url)
@@ -18,6 +21,7 @@ def download_txt(url, filename, folder='books/'):
     with open(filepath, 'wb') as file:
         file.write(response.content)
 
+
 def download_image(image_url, folder='images/'):
     os.makedirs(folder, exist_ok=True)
     response = requests.get(image_url)
@@ -25,23 +29,55 @@ def download_image(image_url, folder='images/'):
     check_for_redirect(response)
     image_name = urlsplit(image_url).path.split('/')[-1]
     filepath = os.path.join(folder, image_name)
-    with open(filepath, 'wb') as file:
+    with open(unquote(filepath), 'wb') as file:
         file.write(response.content)
 
-for id in range(1,10):
-    try:
-        url = f"https://tululu.org/b{id}/"
-        response = requests.get(url)
-        response.raise_for_status() 
-        check_for_redirect(response)
-        soup = BeautifulSoup(response.text, 'lxml')
-        book_image_url = soup.find('div', class_='bookimage').find('img')['src']
-        full_image_url = urljoin('https://tululu.org', book_image_url)
-        download_image(full_image_url)
-        title = soup.find('h1').text
-        book_litle, book_author = title.split(' :: ')
-        filename = f'{id}. {book_litle.strip()}'
-        url_txt_book = f'https://tululu.org/txt.php?id={id}'
-        #download_txt(url_txt_book, filename)
-    except:
-        print('книга не найдена')
+
+def parse_book_page(url):
+    response = requests.get(url)
+    response.raise_for_status() 
+    check_for_redirect(response)
+    soup = BeautifulSoup(response.text, 'lxml')
+    book_image_url = soup.find('div', class_='bookimage').find('img')['src']
+    full_image_url = urljoin('https://tululu.org', book_image_url)
+    title = soup.find('h1').text
+    book_title, book_author = title.split(' :: ')
+    comments_book = soup.find_all('div', class_='texts')
+    comments = [comment.find('span', class_='black').text for comment in comments_book]
+    genres_books = soup.find('span', class_='d_book').find_all('a')
+    genres_books = [genre.text for genre in genres_books]
+    book_parameters = {
+        "title": book_title.strip(),
+        "author": book_author.strip(),
+        "image_url": full_image_url,
+        "genre": genres_books,
+        "comments": comments
+    }
+    return book_parameters
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description= "Проект скачивает книги и соответствующие им картинки,\
+                     а также собирает информацию о книге"
+    )
+    parser.add_argument("--start_id", type=int,
+                        help="Стартовый id книги для скачивания", default=1)
+    parser.add_argument("--end_id", type=int,
+                        help="Конечный id книги для скачивания", default=10)
+    args = parser.parse_args()
+    for id in range(args.start_id, args.end_id):
+        try:
+            url = f"https://tululu.org/b{id}/"
+            book_image_url = parse_book_page(url)['image_url']
+            download_image(book_image_url)
+            book_title = parse_book_page(url)['title']
+            filename = f'{id}. {book_title.strip()}'
+            url_txt_book = f'https://tululu.org/txt.php?id={id}'
+            download_txt(url_txt_book, filename)
+        except:
+            print('книга не найдена')
+   
+
+if __name__=='__main__':
+    main()
